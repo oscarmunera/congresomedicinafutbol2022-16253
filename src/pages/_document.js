3cf859c7a0946fff57d903687e58752c265bd7fc
Óscar Múnera <43389250+oscarmunera@users.noreply.github.com>
2021-11-15 10:19:12 -0500

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -32,6 +32,15 @@ export default class MyDocument extends Document {
         return (
             <Html {...this.helmetHtmlAttrComponents}>
                 <Head>{this.helmetHeadComponents}</Head>
+                        <script async src="https://www.googletagmanager.com/gtag/js?id=G-YTBVKT4ZJZ"></script>
+                        <script dangerouslySetInnerHTML={{
+                    __html: ` window.dataLayer = window.dataLayer || [];
+                          function gtag(){dataLayer.push(arguments);}
+                          gtag('js', new Date());
+
+                          gtag('config', 'G-YTBVKT4ZJZ');`
+                }} />  
+                     
                 <body {...this.helmetBodyAttrComponents}>
                     <Main />
                     <script src="/js/plugins.js" />
